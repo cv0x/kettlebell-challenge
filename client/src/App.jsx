b100f0e0bd7b724ba01622e80babed66c216029d
cv0x <71874388+cv0x@users.noreply.github.com>
2024-05-05 11:07:10 +0200

    register, login
    create register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
+
 export default function App() {
   return (
     <>
-    <div>
-    <h1>
-    </h1>
-    </div>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="bg-gray-800 text-white text-center">
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+    </nav>
   )
 }

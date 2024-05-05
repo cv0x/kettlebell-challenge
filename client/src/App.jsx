0745f1bdb1e1cef6c967c93bf8c12f7e761b41a2
cv0x <71874388+cv0x@users.noreply.github.com>
2024-05-05 10:20:19 +0200

    base set up
    installed vite, vite test, react js, node js, tailwind css,

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+export default function App() {
+  return (
+    <>
+    <div>
+    <h1>
+    </h1>
+    </div>
+    </>
+  )
+}
---
+++ client/src/tests/exemple.test.jsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+describe("Exemple test", () => {
+    it("should be true", () => {
+        const sum = 1 + 1
+        expect(sum).toEqual(2);
+    });
+})

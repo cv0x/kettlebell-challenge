ca9b96ebcb0932e1c765b12b6ff9f90889136bfd
cv0x <71874388+cv0x@users.noreply.github.com>
2024-05-06 12:53:03 +0200

    connect mongodb
    connect and set up database MongoDB with mongoose

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv').config;
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const {mongoose} = require("mongoose");
+
+// Mongodb connection
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
-app.use('/', require('./routes/authRoutes'));
+app.use("/", require("./routes/authRoutes"));
 
 const port = 8000;
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));

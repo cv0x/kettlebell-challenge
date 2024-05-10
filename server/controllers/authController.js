c3601e69ee0615a6dd1b0bca11dbfa68813e1024
cv0x <71874388+cv0x@users.noreply.github.com>
2024-05-10 18:14:57 +0200

    add JSON web token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const { hashPassword, comparePassword } = require("../helpers/auth");
+const jwt = require("jsonwebtoken");
 
 const test = (req, res) => {
   res.json("test");
@@ -54,7 +55,15 @@ const loginUser = async (req, res) => {
     //check password
     const match = await comparePassword(password, user.password);
     if (match) {
-      res.json("password match");
+      jwt.sign(
+        { email: user.email, id: user._id, name: user.name },
+        process.env.JWT_SECRET,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(user);
+        }
+      );
     }
     if (!match) {
       return res.json({ error: "Wrong password" });
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { mongoose } = require("mongoose");
+const cookieParser = require('cookie-parser')
+
 const app = express();
 
 // Mongodb connection
@@ -12,6 +14,8 @@ mongoose
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/authRoutes"));
 
